449bf1c64417b3019fd9e9895fb65118266fe57f
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2024-05-13 09:05:49 -0600

    Agregado la funcionalidad de pelotas cayendo

--- src/main/java/org/controllers/App.java
+++ src/main/java/org/controllers/App.java
@@ -2,8 +2,10 @@ package org.controllers;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,19 +17,41 @@ public class App extends Application {
 
     private static Scene scene;
 
+
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+
+        Pane root = loadFXML("primary");
+        scene = new Scene(root, 500, 500);
         stage.setScene(scene);
+        PrimaryController controller = new PrimaryController(root);
         stage.show();
+        controller.timeline.play();
+
+
+
+//        Circle circle = new Circle(100,100,10, Color.BLUE);
+//        root.getChildren().add(circle);
+//        Timeline timeline = new Timeline(
+//                new KeyFrame(Duration.millis(50), event -> {
+//                    // Aquí puedes cambiar las coordenadas del círculo
+//                    circle.setCenterX(circle.getCenterX() + 10);
+//                    circle.setCenterY(circle.getCenterY() + 10);
+//                })
+//        );
+//        timeline.setCycleCount(Timeline.INDEFINITE);
+//        timeline.play();
+
+
+
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+    private static Pane loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource( "primary.fxml"));
         return fxmlLoader.load();
     }
 
--- src/main/java/org/controllers/PrimaryController.java
+++ src/main/java/org/controllers/PrimaryController.java
@@ -1,12 +1,94 @@
 package org.controllers;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
 
-public class PrimaryController {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+import java.util.ArrayList;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+import org.Modules.Arista;
+import org.Modules.Song;
+import org.Modules.Tecla;
+import org.Modules.TestGraph;
+
+public class PrimaryController{
+
+    private Pane root;
+    private final Song prueba;
+    public Timeline timeline;
+    private Tecla aux;
+    private final ArrayList<Circle> teclasEnPantalla = new ArrayList<>();
+    public static final Color[] ColoresPosibles = 
+            {Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN, Color.ORANGE};
+    private long time;
+    public PrimaryController(Pane root) {
+        this.prueba = TestGraph.RealizarTest();
+        this.timeline = new Timeline(new KeyFrame(Duration.seconds(1), event -> print()));
+        this.root = root;
+        this.aux = prueba.getTeclas_pulsadas().getVertice(0);
+        this.timeline.setCycleCount((int)prueba.getDuracion());
+        this.time = System.nanoTime() ;
+    }
+
+    public void setRoot(Pane root) {
+        this.root = root;
+    }
+
+    private void print() {
+        
+        int xDef = 0;
+        root.getChildren().clear();
+        Tecla lastTecla = null;
+        long time2 = System.nanoTime();
+        float dt = (float) (time2 - time) / 1000000000.0f;
+        time = time2;
+        
+        System.out.println(dt);
+
+
+        //Primero desplaza todas las que ya existen hacia abajo
+        for (Circle circulo : teclasEnPantalla){
+            circulo.setCenterY(circulo.getCenterY() + circulo.getRadius()*3);
+            if (circulo.getCenterY() < 600){
+                root.getChildren().add(circulo);
+            }
+        }
+        //Eliminamos del ArrayList las teclas que ya no se ven
+        teclasEnPantalla.removeIf(circle -> circle.getCenterY() > 600);
+        
+        //Agregamos las nuevas teclas que deben de aparecer
+        if (aux.getNumAristasAdyacentes() != 0){
+            for (Arista i : aux.getAristasAdyacentes()){
+
+                switch (i.destino().numColor){
+                    case 0:
+                        xDef = 50;
+                        break;
+                    case 1:
+                        xDef = 100;
+                        break;
+                    case 2:
+                        xDef = 150;
+                        break;
+                    case 3:
+                        xDef = 200;
+                        break;
+                    case 4:
+                        xDef = 250;
+                        break;
+                    case 5:
+                        xDef = 300;
+                        break;
+                }
+                Circle circle = new Circle(xDef,30,20,ColoresPosibles[i.destino().numColor]);
+                root.getChildren().add(circle);
+                teclasEnPantalla.add(circle);
+                lastTecla = i.destino();
+            }
+            aux = lastTecla;
+        }
     }
 }
