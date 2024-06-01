c76d487cc8b19b7352a3a0941e0770a99eafb3f2
JostinCG <ma@qtimporta.com>
2024-05-31 19:25:26 -0600

    Interface update

--- src/main/java/org/controllers/selectorController.java
+++ src/main/java/org/controllers/selectorController.java
@@ -14,23 +14,23 @@ public class selectorController {
     private Stage stage;
     private Scene scene;
 
-//    public void back(ActionEvent event) throws IOException {
-//        Pane root = loadFXML("Jugador");
-//        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//
-//
-//    }
-//    public void Song(ActionEvent event) throws IOException {
-//        Pane root = loadFXML("Local");
-//        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//        scene = new Scene(root);
-//        stage.setScene(scene);
-//        stage.show();
-//
-//
-//    }
+    public void back(ActionEvent event) throws IOException {
+        Pane root = loadFXML("Jugador");
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
+    public void Song(ActionEvent event) throws IOException {
+        Pane root = loadFXML("Local");
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+
+    }
 
 }
