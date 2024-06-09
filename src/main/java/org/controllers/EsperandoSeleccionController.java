cc2e6ab1ddc43cdeda4da20497b46e3118ced396
Sergio Eder Cervantes Rincon <al232682@edu.uaa.mx>
2024-06-09 17:14:33 -0600

    Arreglado Multijugador, ahora si funcional

--- src/main/java/org/controllers/EsperandoSeleccionController.java
+++ src/main/java/org/controllers/EsperandoSeleccionController.java
@@ -1,11 +1,9 @@
 package org.controllers;
 
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
